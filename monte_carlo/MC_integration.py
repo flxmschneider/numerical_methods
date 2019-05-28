68b0fb27c9b7f2a2b08b3b89d4b1b7fdca6f4793
Felix <felix-maximilian-schneider@web.de>
2019-05-28 21:08:24 +0200

    Added varianc plot

--- monte_carlo/MC_integration.py
+++ monte_carlo/MC_integration.py
@@ -7,19 +7,26 @@ def f(x):
 
 N_list = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
 results = []
+variances = []
 for N in N_list: 
     counter = 0
+    y_values = []
     for i in range(N):
         x = random.random()
         y = random.random()
         if y < f(x):
             counter+=1
+        y_values.append(f(x))
+    variances.append(np.var(y_values))
     result = counter/N
     results.append(result) 
 
-
-plt.plot(N_list, [np.pi/4]*len(N_list),label='Pi/4')
-plt.plot(N_list, results, label = 'Monte Carlo')
-plt.legend()
-plt.title('Monte Carlo Integration')
+fig, ax = plt.subplots(nrows =1, ncols=2)
+ax[0].plot(N_list, [np.pi/4]*len(N_list),label='Pi/4')
+ax[0].plot(N_list, results, label = 'Monte Carlo')
+ax[0].legend()
+ax[0].set_title('Monte Carlo Integration')
+ax[1].set_title('Variances')
+ax[1].plot(N_list, variances)
+plt.tight_layout()
 plt.show()
