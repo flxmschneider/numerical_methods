ffc3bcd4538e058601b718d4ef30cc955d288929
Felix <felix-maximilian-schneider@web.de>
2019-05-29 17:02:22 +0200

    Added second solution with signifact less variance

--- monte_carlo/MC_integration.py
+++ monte_carlo/MC_integration.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 def f(x):
     return (1+x**2)**-1
 
+def f(y):
+    return 3/(4+4*y**2-2*y-2*y**3)
+
+
 N_list = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
 results = []
 variances = []
@@ -20,6 +24,7 @@ for N in N_list:
     variances.append(np.var(y_values))
     result = counter/N
     results.append(result) 
+    print(np.pi/4 - result)
 
 fig, ax = plt.subplots(nrows =1, ncols=2)
 ax[0].plot(N_list, [np.pi/4]*len(N_list),label='Pi/4')
